0696ae4ba878ea03f166f74d7287cf5cd167fdd5
Martin Konrad <konrad@frib.msu.edu>
2017-02-10 16:17:32 -0500

    Test for joinMapKeys

--- drvFGPDBApp/src/drvFGPDBTests.cpp
+++ drvFGPDBApp/src/drvFGPDBTests.cpp
@@ -58,6 +58,32 @@ public:
 };
 
 
+TEST(joinMapKeys, returnsEmptyStringForEmptyMap) {
+  const map<string, int> aMap;
+  const string separator("--");
+
+  string result = joinMapKeys<int>(aMap, separator);
+
+  ASSERT_THAT(result, Eq(""));
+}
+
+TEST(joinMapKeys, concatenatesMapKeysAndSeparators) {
+  const int arbitraryInt = 0;
+  const string key1("KEY1");
+  const string key2("KEY2");
+  const string key3("KEY3");
+  const map<string, int> aMap = {
+    { key1, arbitraryInt },
+    { key2, arbitraryInt },
+    { key3, arbitraryInt }
+  };
+  const string separator("--");
+
+  string result = joinMapKeys<int>(aMap, separator);
+
+  ASSERT_THAT(result, Eq(key1 + separator + key2 + separator + key3));
+}
+
 //=============================================================================
 // *** WARNING ***
 //
